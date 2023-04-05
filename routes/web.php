8d195610bdde3005a02f88223f0863a6e283936a
Hamza zulfiqar <zulfiqarhamza874@gmail.com>
2023-04-06 03:32:24 +0500

    login functionality done

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,4 +20,6 @@ use Illuminate\Support\Facades\Route;
 // });
 
 
-Route::view('/','login');
+Route::view('/login','login');
+Route::post('/login',[UserController::class,'login']);
+Route::get('/',[ProductController::class, 'index']);
